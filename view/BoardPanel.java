dccc737562c523e7f53f9470fa38f5a71b4fe015
Mikoto Kunori <guzaisann@gmail.com>
2026-03-24 20:59:14 +0900

    View層の盤面描画を分離

---
+++ view/BoardPanel.java
@@ -0,0 +1,90 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPanel;
+
+import controller.LG1Controller;
+import model.LG1Model;
+
+/**
+ * ライフゲームの盤面を表示するパネル。
+ * セルの描画とマウスクリックの受付を担当する。
+ */
+public class BoardPanel extends JPanel {
+
+    /** ライフゲームのモデル層 */
+    private LG1Model model;
+
+    /** ライフゲームのコントローラ層 */
+    private LG1Controller controller;
+
+    /** 盤面のグリッド1マスあたりのサイズpx */
+    private int cellSize = 20;
+
+    /**
+     * 盤面表示用パネルを生成する。
+     * 
+     * @param model 描画対象のモデル
+     */
+    public BoardPanel(LG1Model model) {
+        this.model = model;
+
+        int boardWidth = model.getCols() * cellSize;
+        int boardHeight = model.getRows() * cellSize;
+
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+                int col = e.getX() / cellSize;
+                int row = e.getY() / cellSize;
+
+                // クリックされたセルをControllerに伝える。
+                controller.toggleCell(row, col);
+            }
+        });
+    }
+
+    /**
+     * コントローラを設定する。
+     * 
+     * @param controller コントローラ
+     */
+    public void setController(LG1Controller controller) {
+        this.controller = controller;
+    }
+
+    /**
+     * 盤面を描画する。
+     * 
+     * @param g 描画に使用するGraphicsオブジェクト
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        // 前回の描画内容が残らないようにクリアする。
+        super.paintComponent(g);
+
+        // 描画時のみモデルを参照してセル状態を表示する。
+        for (int r = 0; r < model.getRows(); r++) {
+            for (int c = 0; c < model.getCols(); c++) {
+
+                int x = c * cellSize;
+                int y = r * cellSize;
+
+                if (model.getCell(r, c)) {
+                    g.fillRect(x, y, cellSize, cellSize);
+                } else {
+                    g.drawRect(x, y, cellSize, cellSize);
+                }
+            }
+        }
+    }
+}
--- view/LG1View.java
+++ view/LG1View.java
@@ -1,26 +1,24 @@
 package view;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import model.LG1Model;
 import controller.LG1Controller;
+import model.LG1Model;
 
 /**
- * LifeGame1Go の画面表示を担当するビュークラス。
- * 盤面の描画とユーザー入力を処理する。
+ * LifeGame1Go の画面全体を表すビュークラス。
+ * 盤面パネルと操作ボタンを配置する。
  */
 public class LG1View extends JPanel {
-    private int cellSize = 20; // px
-    private JPanel boardPanel;
+
+    /** ライフゲームのコントローラ層 */
     private LG1Controller controller;
-    private LG1Model model;
+
+    /** ライフゲームの盤面パネル */
+    private BoardPanel boardPanel;
 
     /**
      * ビューを生成する。
@@ -28,59 +26,34 @@ public class LG1View extends JPanel {
      * @param model 描画対象のモデル
      */
     public LG1View(LG1Model model) {
-        this.model = model;
 
         setLayout(new BorderLayout());
-        boardPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                // 描画前に背景クリアしておかないと前の描画が残ることがあるらしい
-                super.paintComponent(g);
-                // 描画時のみモデルを参照してセル状態を表示する
-                for (int r = 0; r < model.getRows(); r++) {
-                    for (int c = 0; c < model.getCols(); c++) {
-                        int x = c * cellSize;
-                        int y = r * cellSize;
 
-                        if (model.getCell(r, c)) {
-                            g.fillRect(x, y, cellSize, cellSize);
-                        } else {
-                            g.drawRect(x, y, cellSize, cellSize);
-                        }
-                    }
-                }
-            }
-        };
-
-        boardPanel.setPreferredSize(new Dimension(600, 600));
-        boardPanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                int col = e.getX() / cellSize;
-                int row = e.getY() / cellSize;
-
-                controller.toggleCell(row, col);
-            }
-        });
-
+        boardPanel = new BoardPanel(model);
         add(boardPanel, BorderLayout.CENTER);
 
         JButton startButton = new JButton("Start");
-        JButton stopButton = new JButton("Stop");
-        JButton randomButton = new JButton("Random");
-        JButton clearButton = new JButton("Clear");
-        JButton gliderButton = new JButton("Glider");
-        clearButton.addActionListener(e -> controller.clear());
-        randomButton.addActionListener(e -> controller.random());
         startButton.addActionListener(e -> controller.start());
+
+        JButton stopButton = new JButton("Stop");
         stopButton.addActionListener(e -> controller.stop());
+
+        JButton randomButton = new JButton("Random");
+        randomButton.addActionListener(e -> controller.random());
+
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(e -> controller.clear());
+
+        JButton gliderButton = new JButton("Glider");
         gliderButton.addActionListener(e -> controller.glider());
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(startButton);
         buttonPanel.add(stopButton);
         buttonPanel.add(randomButton);
         buttonPanel.add(clearButton);
         buttonPanel.add(gliderButton);
+
         add(buttonPanel, BorderLayout.SOUTH);
     }
 
@@ -91,12 +64,13 @@ public class LG1View extends JPanel {
      */
     public void setController(LG1Controller controller) {
         this.controller = controller;
+
+        // 盤面パネルにも同じコントローラを渡す。
+        boardPanel.setController(controller);
     }
 
     /**
-     * コントローラを設定する。
-     *
-     * @param controller コントローラ
+     * 盤面を再描画する。
      */
     public void repaintBoard() {
         boardPanel.repaint();
